a0df18edc351c8939a2c62a0f990906a3e319f41
xufengguang <xufengguang>
2019-03-01 17:45:56 +0800

    1.增加工作流查询等测试方法

--- src/test/java/com/main/activity/ActivityApplicationTests.java
+++ src/test/java/com/main/activity/ActivityApplicationTests.java
@@ -1,12 +1,15 @@
 package com.main.activity;
 
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -40,12 +43,48 @@ public class ActivityApplicationTests {
     }
 
 
+    /**
+     * @description 删除工作流
+     * @date 2019/3/1 17:18
+     * @param
+     * @return void
+     */
     @Test
     public void deleteActivitiTask(){
         //第一个参数是部署的流程的ID，第二个true表示是进行级联删除
         repositoryService.deleteDeployment("1", true);
     }
 
+    /**
+     * @description 查询流程信息（act_re_deployment）
+     * @date 2019/3/1 17:36
+     * @param
+     * @return void
+     */
+    @Test
+    public void queryDeploymentByName(){
+        List<Deployment> deployments = repositoryService.createDeploymentQuery().orderByDeploymenTime().desc().deploymentId("2501").list();
+        for (Deployment deployment : deployments) {
+            System.out.println("流程id："+deployment.getId());
+        }
+    }
 
-
+    /**
+     * @description 查询流程定义（act_re_procdef）
+     *              在创建流程时整个流程定义的id（key）和name等
+     * @date 2019/3/1 17:42
+     * @param
+     * @return void
+     */
+    @Test
+    public void testQueryAllPD() {
+        List<ProcessDefinition> pdList = repositoryService
+                .createProcessDefinitionQuery()
+                .orderByProcessDefinitionVersion()
+                .desc()
+                .list();
+        for (ProcessDefinition pd : pdList) {
+            System.out.println(pd.getName());
+        }
+    }
 }
